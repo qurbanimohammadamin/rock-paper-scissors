1224a52055af8787479c648f5f815bc8bffe43b6
Mohammad Amin <122115589+qurbanimohammadamin@users.noreply.github.com>
2023-09-21 01:55:13 -0400

    Add getComputerChoice function in javaScript.js
    getComputerChoice function get the computer choice(rock, paper, scissors) randomly.

--- javaScript.js
+++ javaScript.js
@@ -1,39 +1,3 @@
-
-alert(checkResult(playerSelection(),getComputerChoice()));
-
-
-
-function checkResult(userSelection, computerSelection){
-    let userSelectionToLowerCase = userSelection.toLowerCase();
-    let result = "";
-    if(userSelectionToLowerCase === computerSelection){result = "equal!";}
-    else if(userSelectionToLowerCase === "rock"){
-        if(computerSelection === "paper"){result = "you lose!"}
-        else result = "you win!";
-    }
-    else if( userSelectionToLowerCase === "paper"){
-        if(computerSelection === "scissors"){result = "you lose!"}
-        else result = "you win!";
-    }
-    else if(userSelectionToLowerCase === "scissors"){
-        if(computerSelection==="rock"){result = "you lose!"}
-        else result = " you win!"
-    }
-     else result = "spell not correct!";
-     return result; 
-    
-}
-
-
-
-function playerSelection(){
-    let result = prompt("inter your choice:");
-    return result;
-}
-
-
-
-
 function getComputerChoice(){
     let result;
     let random = Math.random() * 100;
@@ -42,4 +6,3 @@ function getComputerChoice(){
     else {result = "scissors";}
     return result;
 }
-
